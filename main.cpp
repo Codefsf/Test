ca445f509d89745a6c0493531e3b331097b10078
guoxudong01 <guoxudong01@corp.netease.com>
2021-11-29 12:08:33 +0800

    增加创建db功能

--- main.cpp
+++ main.cpp
@@ -3,9 +3,15 @@
 #include <algorithm>
 #include <memory>
 
+#include "leveldb/db.h"
+#include "leveldb/env.h"
+
 int main( void )
 {
-
+    leveldb::DB* db = nullptr;
+    leveldb::Options options;
+    options.create_if_missing = true;
+    leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
 
     return 0;
 }
