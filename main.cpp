3c2bd555d74b960e429fdfe6a3d064e143368381
guoxudong01 <guoxudong01@corp.netease.com>
2021-11-29 14:07:17 +0800

    增加leveldb读写

--- main.cpp
+++ main.cpp
@@ -8,10 +8,29 @@
 
 int main( void )
 {
+    //打开数据库
     leveldb::DB* db = nullptr;
     leveldb::Options options;
     options.create_if_missing = true;
-    leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
+    leveldb::Status status = leveldb::DB::Open(options, "./testdb", &db);
+
+    //读写数据库
+    std::string value1 = "testvalue1";
+    std::string value2 = "testvalue2";
+    std::string value3 = "testvalue3";
+
+    status = db->Put(leveldb::WriteOptions(), "key1", value1);
+    status = db->Put(leveldb::WriteOptions(), "key2", value2);
+    status = db->Put(leveldb::WriteOptions(), "key3", value3);
+
+
+    leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+    for (it->SeekToFirst(); it->Valid(); it->Next()) {
+        std::cout << it->key().ToString() << ": "  << it->value().ToString() << std::endl;
+    }
+    delete it;
+    //关闭数据库
+    delete db;
 
     return 0;
 }
