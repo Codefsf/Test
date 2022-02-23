c1c7e67fb467d13edaab2e91e1bdbd931431e429
[guoxudong] <[510105194@qq.com]>
2022-02-23 12:03:47 +0800

    feat:add thread feature

--- test_memory.hpp
+++ test_memory.hpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <memory>
 #include <mutex>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 class TestMutex
 {
 public:
-    TestMutex()
+    TestMutex(const std::string& name) : 
+        name_(name)
     {
         cout << "TestMutex()" << endl;
     }
@@ -27,17 +30,38 @@ public:
     {
         std::lock_guard<std::mutex> lock(mutex_);
         value_ = value;
+        cout << name_ << " = " << "setValue:" << std::to_string(value) << endl;
     }
 
 private:
-    int64_t value_ {0};
+    std::string     name_ {nullptr};
+    int64_t         value_ {0};
     mutable std::mutex mutex_;
 };
 
 void TestMemory()
 {
-    TestMutex test_mutex;
-    test_mutex.setValue(1);
-    std::cout << test_mutex.getValue() << std::endl;
+    TestMutex test_mutex("task-1");
+
+    std::thread t1([&test_mutex]() {
+        for (int i = 0; i < 100; ++i)
+        {
+            test_mutex.setValue(i);
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    });
+
+    std::thread t2([&test_mutex]() {
+        for (int i = 0; i < 100; ++i)
+        {
+            test_mutex.setValue(i);
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    });
+
+    t1.join();
+    t2.join();
 }
 
